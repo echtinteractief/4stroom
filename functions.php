bb50433a9e481e648b6f356b69371dee237d851c
Berry <berry.wijnia@avrotros.nl>
2015-06-24 16:36:54 +0200

    no message

--- functions.php
+++ functions.php
@@ -144,8 +144,8 @@ function bones_theme_customizer($wp_customize) {
   // Uncomment the below lines to remove the default customize sections 
 
   // $wp_customize->remove_section('title_tagline');
-  // $wp_customize->remove_section('colors');
-  // $wp_customize->remove_section('background_image');
+  $wp_customize->remove_section('colors');
+  $wp_customize->remove_section('background_image');
   // $wp_customize->remove_section('static_front_page');
   // $wp_customize->remove_section('nav');
 
@@ -153,8 +153,8 @@ function bones_theme_customizer($wp_customize) {
   // $wp_customize->remove_control('blogdescription');
   
   // Uncomment the following to change the default section titles
-  // $wp_customize->get_section('colors')->title = __( 'Theme Colors' );
-  // $wp_customize->get_section('background_image')->title = __( 'Images' );
+  //$wp_customize->get_section('colors')->title = __( 'Theme Colors' );
+  //$wp_customize->get_section('background_image')->title = __( 'Images' );
 }
 
 add_action( 'customize_register', 'bones_theme_customizer' );
