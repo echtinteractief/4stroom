860ea6f39f7ae5ab0e59588e6eaf8b6dd6a26c6d
Berry <berry.wijnia@avrotros.nl>
2015-06-26 11:28:18 +0200

    wens hvdh is footer sitemap zelf kunnen beheren, vandaar footer_links menu aangemaakt

--- footer.php
+++ footer.php
@@ -1,98 +1,57 @@
 <footer class="footer-page">
-		<div class="row">
-			<div class="grid-8">
-			<ul class="sitemap group-3">
-
-				<?php
-				$args = array(
-						'posts_per_page' => 3,
-						'order_by' => 'title',
-						'order' => 'ASC',
-						'tag' => 'thuissite',
-						'post_type' => 'page'
-				);
-				$the_query = new WP_Query($args); ?>
-
-				<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-
-				<li class="sitemap-item">
-					<a href="<?php echo get_permalink() ?>"><?php the_title(); ?></a>
-
-					<?php
-					$my_wp_query = new WP_Query();
-					$all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));
-
-					$args = array(
-							'child_of' => $post->ID,
-							'parent' => $post->ID,
-							'sort_column' => 'menu_order',
-							'sort_order' => 'asc'
-					);
-					$subpages = get_pages( $args );
-
-					if(count($subpages) > 0){ ?>
-						<ul class="sitemap-subgroup">
-						<?php foreach($subpages as $subpage){ ?>
-							<li class="sitemap-subitem"><a href="<?php echo $subpage->post_title; ?>"><?php echo $subpage->post_title; ?></a></li>
-						<?php } ?>
-						</ul>
-					<?php }
-					?>
-				</li>
-
-
-					<?php endwhile;
-						wp_reset_postdata(); ?>
-
-				<li class="sitemap-item">
-					<a href="#">Leden</a>
-					
-					<ul class="sitemap-subgroup">
-						<li class="sitemap-subitem"><a href="#">link to</a></li>
-					</ul>
-				</li>
-				<li class="sitemap-item">
-					<a href="#">Vierstroom</a>
-					
-					<ul class="sitemap-subgroup">
-						<li class="sitemap-subitem"><a href="#">link to</a></li>
-					</ul>
-				</li>
-			</ul>
-			</div>
-			<div class="grid-4">
-				<div class="box newsletter-submit widget">
-					<h1 class="widget-title">Nog geen Vierstroom nieuwsbrief?</h1>
-					<div class="text">
-						<p>
-							We houden u op deze wijze graag op de hoogte van de zorg- en dienstverlening van Vierstroom.
-						</p>
-					</div>
-					<form>
-						<input type="submit" class="btn lighter send " value="&#xf0e0; Aanmelden" />
-					</form>
-					
+	<div class="row">
+		<div class="grid-8">
+			
+			<?php wp_nav_menu(array(
+		         'container' => false,                           // remove nav container
+		         'container_class' => '',                 // class of container (should you choose to use it)
+		         'menu' => __( 'Footer Sitemap', 'bonestheme' ),  // nav name
+		         'menu_class' => 'sitemap group-3',               // adding custom nav class
+		         'theme_location' => 'footer-links',                 // where it's located in the theme
+		         'before' => '',                                 // before the menu
+				 'after' => '',                                  // after the menu
+				 'link_before' => '',                            // before each link
+	             'link_after' => '',                             // after each link
+				 'depth' => 2,                                   // limit the depth of the nav
+				 'fallback_cb' => '',                             // fallback function (if there is one)
+				 'walker' => new Footer_sitemap_nav
+			)); 
+			
+			?>	
+		</div>
+		<div class="grid-4">
+			<div class="box newsletter-submit widget">
+				<h1 class="widget-title">Nog geen Vierstroom nieuwsbrief?</h1>
+				<div class="text">
+					<p>
+						We houden u op deze wijze graag op de hoogte van de zorg- en dienstverlening van Vierstroom.
+					</p>
 				</div>
-				<nav class="social-follow-nav widget">
-					<h1 class="widget-title">Volg ons op:</h1>
-					<ul>
-						<li><a href="#facebook" class="btn-round icon icon-only icon-facebook">Facebook</a></li>
-						<li><a href="#twitter" class="btn-round  icon icon-only icon-twitter">Twitter</a></li>
-						<li><a href="#youtube" class="btn-round  icon icon-only icon-youtube">Youtube</a></li>
-						<li><a href="#linkedin" class="btn-round icon icon-only icon-linkedin">LinkedIn</a></li>
-					</ul>
-				</nav>
-				
+				<form>
+					<input type="submit" class="btn lighter send " value="&#xf0e0; Aanmelden" />
+				</form>
 				
 			</div>
+			<nav class="social-follow-nav widget">
+				<h1 class="widget-title">Volg ons op:</h1>
+				<ul>
+					<li><a href="http://www.facebook.com/vierstroom" target="_blank" class="btn-round icon icon-only icon-facebook">Facebook</a></li>
+					<li><a href="http://www.twitter.com/" target="_blank"  class="btn-round  icon icon-only icon-twitter">Twitter</a></li>
+					<li><a href="http://www.youtube.com/user/Vierstroom" target="_blank"  class="btn-round  icon icon-only icon-youtube">Youtube</a></li>
+					<li><a href="http://www.linkedin.com/" target="_blank"  class="btn-round icon icon-only icon-linkedin">LinkedIn</a></li>
+				</ul>
+			</nav>
+			
+			
 		</div>
-	</footer>
-	<footer class="footer-page-end">
-		<div class="row">
-			<span class=""><?php echo date('Y'); ?>  </span>
-			<em class=""><?php bloginfo('name'); ?>, <?php bloginfo('description');?></em>
-		</div>
-	</footer>
+	</div>
+</footer>
+<footer class="footer-page-end">
+	<div class="row">
+		<span class=""><?php echo date('Y'); ?>  </span>
+		<em class=""><?php bloginfo('name'); ?>, <?php bloginfo('description');?></em>
+	</div>
+</footer>
 	
 	
 <?php wp_footer(); ?>
