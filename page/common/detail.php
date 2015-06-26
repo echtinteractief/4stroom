691e85415aff86baf5285766c22a1480e4f43e1a
Gert-Jan Berghuis <gertjanberghuis@gmail.com>
2015-06-27 01:06:13 +0200

    Teaser implementation

--- page/common/detail.php
+++ page/common/detail.php
@@ -26,6 +26,49 @@
 				the_content();
 				?>
 			</div>
+
+			<!-- Teaser groups -->
+			<?php if( have_rows('teasers') ): ?>
+				<?php while( have_rows('teasers') ): the_row(); ?>
+
+					<h2 class="page-heading"><?php the_sub_field('groep_titel'); ?></h2>
+						<ul class="group-2 box">
+<?php
+							$posts = get_sub_field('paginas');
+
+							if( $posts ): ?>
+							<ul>
+								<?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+									<?php setup_postdata($post); ?>
+									<?php
+										$title= get_field('teaser_titel', $post->ID)  ? get_field('teaser_titel', $post->ID) : get_the_title($post->ID);
+										$content= get_field('teaser_beschrijving', $post->ID) ? get_field('teaser_beschrijving', $post->ID) : get_the_excerpt($post->ID);
+										$img= get_field('teaser_afbeelding', $post->ID)['url'] ? get_field('teaser_afbeelding', $post->ID)['url'] : wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),'thumb-300');
+										$uri =  get_page_link($post->ID); ?>
+
+										<li class="post post-text-in-image shadow">
+											<article>
+												<figure class="crop">
+													<img src="<?php echo $img ?>">
+												</figure>
+												<div class="text-block">
+													<h1><?php echo $title ?>
+													</h1>
+													<p class="fixed-height"><em><?php echo $content ?></em></p>
+												</div>
+												<a href="<?php echo $uri ?>" class="box-link">Lees meer</a>
+											</article>
+										</li>
+
+								<?php endforeach; ?>
+							</ul>
+							<?php wp_reset_postdata(); ?>
+							<?php endif; ?>
+						</ul>
+
+					<?php endwhile; ?>
+			<?php endif; ?>
+
 		</div>
 		
 		<aside class="grid-4 sidebar">
