83619d49355b3dffe27dce232182c8d648e244ff
Berry <berry.wijnia@avrotros.nl>
2015-06-28 15:05:33 +0200

    maniak kleine aanpassing voor de niet services pagina;s

--- parts/services/nav.php
+++ parts/services/nav.php
@@ -1,8 +1,26 @@
+
+
 <aside class="bar contact-bar ">
 	<div class="row">
 		<h1 class="bar-title arrow-vierstroom right"><span>Direct contact? Bel </span><a href="#link">088 - 0900 400</a></h1>
+		<?php
+			$mother_id = get_top_parent_page_id();
+			
+			$serviceTitle= get_the_title($mother_id);
+			
+			if($serviceTitle=='ZorgThuis' || $serviceTitle=='HulpThuis' || $serviceTitle=='VeiligThuis') :
+		?>
+		
+		
 		<nav class="nav main-nav left tabs">
 			<h1 class="hide">Onze diensten:</h1>
+			
+			<?php else : ?>
+			
+			<nav class="nav main-nav left services">
+				<h1 class="">Onze diensten:</h1>
+			
+			<?php endif; ?>
 			
 				<?php wp_nav_menu(array(
 						'container' => false,                           // remove nav container
