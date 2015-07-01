b408e6db4ba9542cb6dda9d522cbf6227c29a434
Berry <info@berrywijnia.nl>
2015-07-02 00:37:08 +0200

    no message

---
+++ single-ledenaanbieding.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Template Name: Leden page template
+*/
+?>
+
+<!doctype html>
+<!--[if lt IE 7 ]>
+<html class="ie ieLT ie6" lang="nl"> <![endif]-->
+<!--[if IE 7 ]>
+<html class="ie ieLT ie7" lang="nl"> <![endif]-->
+<!--[if IE 8 ]>
+<html class="ie ieLT ie8" lang="nl"> <![endif]-->
+<!--[if IE 9 ]>
+<html class="ie ieLT ie9" lang="nl"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="nl">
+<head>
+	<?php
+	get_header();
+
+	//body page class
+	$page_slug = "";
+	if ( is_page() ) {
+		$page_slug = 'ledenPage page-' . $post->post_name;
+	}
+	?>
+</head>
+<body <?php body_class($page_slug); ?>>
+
+<?php
+get_template_part( 'parts/members/leden-topnav' );
+get_template_part( 'parts/members/leden-header-img' );
+?>
+
+<article>
+	<?php get_template_part( 'parts/members/leden-mainnav' ); ?>
+
+	
+		
+	<?php 
+	
+		
+		
+			get_template_part('page/members/action-detail');
+		 
+	
+	?>
+
+	
+</article>
+
+<?php get_footer(); ?>
+</body>
+</html>
