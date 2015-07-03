749a2788002d96b019a09d5f019ff0d7ec18f2e4
Berry <info@berrywijnia.nl>
2015-07-03 09:15:26 +0200

    diverse bug fixen

--- page/members/detail.php
+++ page/members/detail.php
@@ -1,10 +1,14 @@
 <div class="box block-padding">
 	<div class="row ">
-		<div class="">
+		<div class="grid-8">
 			<h1 class="page-heading"><?php echo get_the_title(); ?></h1>
 			<div class="text">
 				<?php the_content(); ?>
 			</div>
+		</div>
+		
+		<div class="grid-4">
+			&nbsp;
 		</div>
 		
 	</div>
