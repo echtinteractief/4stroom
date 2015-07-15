1bb87affcd2a97c029073a4357243e769b1e4bd8
Berry <info@berrywijnia.nl>
2015-07-15 09:15:06 +0200

    zoeken en 404 werkend gemaakt, nog niet gestyled

--- 404.php
+++ 404.php
@@ -5,13 +5,40 @@
 <!--[if IE 9 ]><html class="ie ieLT ie9" lang="nl"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!-->
 <html lang="nl">
-<head>
+	<head>
 	<?php
 		get_header();	
 	?>
-</head>
-	<body class="page_404">
+	
+	<?php
+		//body page class
+		$page_slug="";
+		if(is_page()) { $page_slug = 'page-'.$post->post_name; } 
+	?>
+	</head>
+	<body <?php body_class($page_slug); ?>>
+		<?php get_template_part('parts/common/topnav'); ?>
+		
+		<?php get_template_part('parts/common/_part-header-img'); ?>
+		
+		<article>
+		
+		
+			<div class="row block-padding">
+				<h1 class="page-heading">Oeps de pagina bestaat niet (meer)</h1>
+				
+				
+				<div class="text">
 
+					<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'bonestheme' ); ?></p>
+
+				</div>
+
+				<?php get_search_form(); ?>
+
+
+			</div>
+		</article>
 
 	<?php get_footer(); ?>
 	</body>
