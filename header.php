f1d57b44c1738824d86bbd26d1447fd70f08b32e
Berry <info@berrywijnia.nl>
2015-07-18 10:39:16 +0200

    viewport scale eruit gehaald ivm niet responsive site

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+<?php 
+	//<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" /> 
+?>
 <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 <?php wp_head(); ?>
 <title>
@@ -14,3 +16,13 @@
 </title>
 
 <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/library/style/gfx/favicon.png">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-29464609-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
