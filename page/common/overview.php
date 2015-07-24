9539c8797ddb06f81903bf8cfc5aa4fc73fc49e4
Gert-Jan Berghuis <gertjanberghuis@gmail.com>
2015-07-24 12:51:28 +0200

    Show categories of a post in the overview

--- page/common/overview.php
+++ page/common/overview.php
@@ -1,90 +1,94 @@
+<div class="grid-8 content ">
+	<h1 class="page-heading"><?php the_title(); ?></h1>
 
-	<div class="grid-8 content ">
-		<h1 class="page-heading"><?php the_title(); ?></h1>
+	<div class="text">
+		<p class="intro">
+			<strong>
 
-		<div class="text">
-			<p class="intro">
-				<strong>
+				<?php the_content(); ?>
 
-					<?php the_content(); ?>
-
-				</strong>
-			</p>
-		</div>
-		<section class="block list-block">
-			<h2 class="hide page-heading"><?php echo get_the_title(); ?> overzicht:</h2>
+			</strong>
+		</p>
+	</div>
+	<section class="block list-block">
+		<h2 class="hide page-heading"><?php echo get_the_title(); ?> overzicht:</h2>
 
 
-				<?php
-				$args = array(
-						'page_type' => 'post');
+		<?php
+		$args = array(
+				'page_type' => 'post' );
 
-				$news = new WP_Query( $args );
-				if( $news->have_posts() ) { ?>
+		$news = new WP_Query( $args );
+		if ( $news->have_posts() ) {
+			?>
 
-				<?php
-				$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-				$wp_query = new WP_Query( array( 'page_type' => 'post', 'posts_per_page' => 5, 'paged' => $paged ) );
+			<?php
+			$paged    = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+			$wp_query = new WP_Query( array( 'page_type' => 'post', 'posts_per_page' => 5, 'paged' => $paged ) );
 
-				// check if category is selected
-				$currentCategory = $_GET['category'];
-					if ( $currentCategory ) {
-					$args     = array(
-							'category_name' => $currentCategory,
-							'page_type'          => 'post',
-							'paged'							 => $paged,
-							'posts_per_page' => 5
-					);
-					$wp_query = null;
-					$wp_query = new WP_Query( $args );
-				}?>
+			// check if category is selected
+			$currentCategory = $_GET['category'];
+			if ( $currentCategory ) {
+				$args     = array(
+						'category_name'  => $currentCategory,
+						'page_type'      => 'post',
+						'paged'          => $paged,
+						'posts_per_page' => 5
+				);
+				$wp_query = null;
+				$wp_query = new WP_Query( $args );
+			}?>
 
 
 			<ul id="inner-content" class="list-view">
 				<?php
-				//query_posts( 'posts_per_page=5' );
 
-				if (have_posts()) : while (have_posts()) : the_post();
+				if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-					$title=(types_render_field("teaser-title",array('raw' => 'true'))) ? types_render_field("teaser-title",array('raw' => 'true')) : get_the_title();
-					$content=(types_render_field("teaser-txt",array('raw' => 'true'))) ? types_render_field("teaser-txt",array('raw' => 'true')) : get_the_excerpt();
+					$title   = ( types_render_field( "teaser-title", array( 'raw' => 'true' ) ) ) ? types_render_field( "teaser-title", array( 'raw' => 'true' ) ) : get_the_title();
+					$content = ( types_render_field( "teaser-txt", array( 'raw' => 'true' ) ) ) ? types_render_field( "teaser-txt", array( 'raw' => 'true' ) ) : get_the_excerpt();
 
-					$content = strip_tags($content);//remove html tags
+					$content = strip_tags( $content ); //remove html tags
 
-					if(strlen($content)>200)
-						$content = substr($content, 0, 200).'...';
+					if ( strlen( $content ) > 200 )
+						$content = substr( $content, 0, 200 ) . '...';
 
-
-// 								$img=(types_render_field("teaser-img",array('raw' => 'true'))) ? types_render_field("teaser-img",array('raw' => 'true')) : wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),'thumb-300');
-					$uri =  get_permalink($post->ID);
+					$uri = get_permalink( $post->ID );
 					?>
-					<li  class="post post-archive type-">
+					<li class="post post-archive type-">
+						<!-- Show the categories of the current post if there's not been a category filtered -->
+						<?php
+						if ( ! isset( $currentCategory ) ) {
+							$postCategories = get_the_category( $post->ID );
+							foreach ( $postCategories as $index => $cat ) {
+								if ( $index != 0 ) {
+									echo ', ' . $cat->name;
+								} else {
+									echo $cat->name;
+								}
+							}
+						}
+						?>
 						<article>
 
 							<div class="meta date">
 								<div class="column">
-									<span class="meta-day"><?php the_time('d'); ?></span>
-									<span class="meta-month"><?php the_time('M'); ?></span>
-									
+									<span class="meta-day"><?php the_time( 'd' ); ?></span>
+									<span class="meta-month"><?php the_time( 'M' ); ?></span>
+
 								</div>
 							</div>
 
-							
-								<?php /* if($img) : ?>
-							<figure class="crop round left">
-								<img src="<?php	echo $img;?>" />
-							</figure>
-							<?php endif; */?>
+							<div class="text-block">
 
-								<div class="text-block">
-									
-									<h1 class="post-title"><?php echo $title; ?></h1>
-									<p class="post-text fixed-height">
-										<?php echo $content; ?>
-									</p>
-								</div>
-							
-							<a class="box-link" href="<?php echo $uri;?>">lees meer over <?php echo $title;?></a>
+								<h1 class="post-title"><?php echo $title; ?></h1>
+
+								<p class="post-text fixed-height">
+									<?php echo $content; ?>
+								</p>
+							</div>
+
+							<a class="box-link" href="<?php echo $uri; ?>">lees meer over <?php echo $title; ?></a>
 						</article>
 
 
@@ -92,22 +96,19 @@
 				<?php endwhile; endif; ?>
 			</ul>
 
-			<?php }
-			else {
-				echo 'Geen nieuws op dit moment.';
-			}
-			?>
+		<?php
+		} else {
+			echo 'Geen nieuws op dit moment.';
+		}
+		?>
 
-			<?php
-			//if ( $wp_query->max_num_pages > 1 ) :
-			//bones_page_navi();
-			bones_page_navi( '', '', $wp_query );
-			//endif;
-			?>
-		</section>
-	</div>
+		<?php
+		bones_page_navi( '', '', $wp_query );
+		?>
+	</section>
+</div>
 
-	<aside class="grid-4 sidebar">
-		<?php get_template_part('parts/common/subnav'); ?>
-	</aside>
+<aside class="grid-4 sidebar">
+	<?php get_template_part( 'parts/common/subnav' ); ?>
+</aside>
 
