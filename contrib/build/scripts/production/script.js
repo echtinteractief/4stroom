1ac959bc9feef20b503a4487776bef7d13be5d1a
Berry <info@berrywijnia.nl>
2015-08-01 09:55:56 +0200

    diverse tweaks

--- contrib/build/scripts/production/script.js
+++ contrib/build/scripts/production/script.js
@@ -691,7 +691,7 @@ $(function(){
 	
 	
 	// double click fix
-	$('nav a').on('touchend', function(e) {
+	$('nav a').on('click', function(e) {
 	  var el = $(this);
 	  var link = el.attr('href');
 	  window.location = link;
@@ -702,46 +702,5 @@ $(function(){
         changeMonth: true,
 		changeYear: true
     });	
-   
-		
-/*
-	if(slider.checkDom('.hasDatepicker')) {
-		
-		$( '.hasDatepicker' ).datepicker({
-    	    changeMonth: true,
-			changeYear: true
-    	});
-    }
-	
-*/
-/*
-	if(slider.checkDom('#Gmap')) {
-		
-		var map=new GMaps({
-				div: '#Gmap',
-				lat: 52.5142307,
-				lng: 6.1069978,
-				zoom:9
-			});
-		map.addMarker({
-		  lat: 52.362536,
-		  lng: 5.652324,
-		  title: 'Van Rijn',
-		  infoWindow: {
-		  	content: '<a href="https://www.google.nl/maps/place/Nobelstraat+25,+3846+CE+Harderwijk/@52.362536,5.652324,17z/data=!3m1!4b1!4m2!3m1!1s0x47c6324f16b46c2b:0xa3bccd495d8199ba" target="_blank">Van Rijn,<br /> Bekijk op google maps</a>'
-        	}
-		});
-		
-		map.addMarker({
-		  lat: 52.6010168,
-		  lng: 6.0985107,
-		  title: 'Kraanverhuur Dick ten Klooster',
-		  infoWindow: {
-		  	content: '<a href="https://www.google.nl/maps/place/Zwartsluizerweg+11,+8061+AB+Hasselt/@52.6010168,6.0985107,17z/data=!4m7!1m4!3m3!1s0x47c8769d15caa21b:0x87d25b84eda86e60!2sZwartsluizerweg+11,+8061+AB+Hasselt!3b1!3m1!1s0x47c8769d15caa21b:0x87d25b84eda86e60" target="_blank">Kraanverhuur Dick ten Klooster, <br />Bekijk op google maps</a>'
-        	}
-		});
-
-	}
-*/	
 
 });//end line
