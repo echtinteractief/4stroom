1ed584f83cd4a0f72c58e1fbaf0b9afb60fd46ea
Berry <info@berrywijnia.nl>
2015-08-07 12:18:56 +0200

    mogelijkheid om de stempel ook uit te zetten ingebouwd

--- parts/members/leden-actionstamp.php
+++ parts/members/leden-actionstamp.php
@@ -1,11 +1,14 @@
+<?php
+	// Get the id of member home
+	$mother_id = get_top_parent_page_id(); //get mother ancestor id
+?>
+
+<?php if(get_field('leden_actie_stempel', $mother_id)): ?>
 <div class="action-stamp widget bg-brand-color">
 	<div class="action-table">
 		<div class="action-content">
 			<article>
-				<?php
-					// Get the id of member home
-					$mother_id = get_top_parent_page_id(); //get mother ancestor id
-				?>
+				
 				<h1 class="action-title"><?php echo get_field('actionstamp_titel', $mother_id); ?></h1>
 				<p class="action-text"><?php echo get_field('actionstamp_sub_titel', $mother_id); ?></p>
 				<a href="#linkto?" class="box-link">Lees meer</a>
@@ -13,3 +16,4 @@
 		</div>
 	</div>
 </div>
+<?php endif; ?>
