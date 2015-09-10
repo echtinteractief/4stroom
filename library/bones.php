71026e48490a119b1fd99f1d1e07bd177f7dea80
Berry <info@berrywijnia.nl>
2015-09-10 14:39:02 +0200

    vierstroom version js/css

--- library/bones.php
+++ library/bones.php
@@ -129,7 +129,8 @@ function bones_scripts_and_styles() {
 
 		// register main stylesheet
 		
-		wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/style/css/style.min.css', array(), '', 'all' );
+		$version = filemtime(get_stylesheet_directory() . '/library/style/css/style.min.css');
+		wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/style/css/style.min.css?v='.$version, array(), '', 'all' );
 		// ie-only style sheet
 		//wp_register_style( 'bones-ie-only', get_stylesheet_directory_uri() . '/library/css/ie.css', array(), '' );
 
@@ -153,7 +154,8 @@ function bones_scripts_and_styles() {
 		using the google cdn. That way it stays cached
 		and your site will load faster.
 		*/
-		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/scripts/production/script.min.js', array(), '', true );
+		$version = filemtime(get_stylesheet_directory() . '/library/scripts/production/script.min.js');
+		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/scripts/production/script.min.js?v='.$version, array(), '', true );
 
 		//wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'bones-js' );
